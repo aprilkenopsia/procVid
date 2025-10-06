506d4aa67664582636a9625acdf2729a4e07b300
aknp <altkirikiri@gmail.com>
2025-10-06 08:59:34 -0300

    lab2

---
+++ lab2.cpp
@@ -0,0 +1,17 @@
+#include <#iostream>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+Mat src; Mat dst;
+char window_name[] = "Blur Demo";
+
+int display_dst( int delay );
+
+int main(int argc, char ** argv)
+{
+    namedWindow
+}

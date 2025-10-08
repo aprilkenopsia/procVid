0fcf12864ce1f1a594832106deb445deb9d5c254
aknp <altkirikiri@gmail.com>
2025-10-08 10:31:08 -0300

    mais consistente + arrumei o loop

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -13,55 +13,48 @@ int i = 0;
 
 
 
-int main(int argc, char** argv )
+int main(int argc, char** argv)
 {
     printf("Digite o valor de a e b (valores impares): ");
     scanf("%d %d", &arr[0], &arr[1]);
 
-    if ( argc != 2 )
+    if (argc != 2)
     {
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
     }
- while (i < 4) {
+    
     Mat src;
-    src = imread( argv[1], IMREAD_COLOR );
-    if ( !src.data )
+    src = imread(argv[1], IMREAD_COLOR);
+    if (!src.data)
     {
         printf("No image data \n");
         return -1;
     }
-    if (i == 0) {
-        cv::Mat dst;
-        cv::blur(src, dst, Size(arr[0], arr[1]));
-        cv::imwrite("blurred.png", dst);
-        printf("Imagem salva como blurred.png\n");
-        i++;
-    }
-    else if (i == 1) {
-        cv::Mat dst;
-        cv::GaussianBlur(src, dst, Size(arr[0], arr[1]), 0);
-        cv::imwrite("gaussian_blurred.png", dst);
-        printf("Imagem salva como gaussian_blurred.png\n");
-        i++;
-    }
-    if (i == 2) {
-        cv::Mat dst;
-        cv::medianBlur(src, dst, 3);
-        cv::imwrite("median_blurred.png", dst);
-        printf("Imagem salva como median_blurred.png\n");
-        i++;
-    }
-    if (i == 3) {
-        cv::Mat dst;
-        cv::bilateralFilter(src, dst, arr[0], arr[1]*2 , arr[1]/2);
-        cv::imwrite("bilateral_filtered.png", dst);
-        printf("Imagem salva como bilateral_filtered.png\n");
-    }
-    // cv::putText(dst, "imagem salva para blurred.png", cv::Point(20,50), cv::FONT_HERSHEY_COMPLEX, 0., cv::Scalar(255,255,255), 2);
-    // namedWindow("Display Image", WINDOW_AUTOSIZE );
-    // imshow("Display Image", dst);
+    
+    // Blur
+    cv::Mat dst1;
+    cv::blur(src, dst1, Size(arr[0], arr[1]));
+    cv::imwrite("export/blurred.png", dst1);
+    printf("Imagem salva em blurred.png\n");
+    
+    // Gaussian blur
+    cv::Mat dst2;
+    cv::GaussianBlur(src, dst2, Size(arr[0], arr[1]), 0);
+    cv::imwrite("gaussian_blurred.png", dst2);
+    printf("Imagem salva como gaussian_blurred.png\n");
+    
+    // Median blur
+    cv::Mat dst3;
+    cv::medianBlur(src, dst3, arr[0]);
+    cv::imwrite("median_blurred.png", dst3);
+    printf("Imagem salva como median_blurred.png\n");
+    
+    // Bilateral filter
+    cv::Mat dst4;
+    cv::bilateralFilter(src, dst4, arr[0], arr[1]*2, arr[1]/2);
+    cv::imwrite("bilateral_filtered.png", dst4);
+    printf("Imagem salva como bilateral_filtered.png\n");
+    
     return 0;
-
- }
-}                                                 
+}                                 
